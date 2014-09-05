f89f6fc17c40db322ccd6087c7316303d762212d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-05 14:19:01 -0500

    EOL_CHARACTERS, EOL2NAME, and NAME2EOL

--- lib/line-detector.rb
+++ lib/line-detector.rb
@@ -6,24 +6,38 @@ require 'version'
 # LineDetector - line ending detector
 #
 module LineDetector
+  EOL_CHARACTERS = [
+    "\r",
+    "\n",
+    "\v",
+    "\f",
+    "\u2028",
+    "\u2029",
+    "\u0085"
+  ]
+
+  EOL2NAME = {
+    "\r\n" => :crlf,
+    "\n\r" => :lfcr,
+    "\n" => :lf,
+    "\r" => :cr,
+    "\v" => :vt,
+    "\f" => :ff,
+    "\u2028" => :ls,
+    "\u2029" => :ps,
+    "\u0085" => :nel
+  }
+
+  NAME2EOL = EOL2NAME.invert
+
   #
   # Detect line ending format of arbitrary text
   #
-  # "\r\n"   => :crlf
-  # "\n\r"   => :lfcr
-  # "\n"     => :lf
-  # "\r"     => :cr
-  # "\v"     => :vt
-  # "\f"     => :ff
-  # "\u2028" => :ls
-  # "\u2029" => :ps
-  # "\u0085" => :nel
-  # "\na\r"  => :mix
-  # "a"      => :none
-  # ""       => :none
+  # If text uses multiple line ending formats,
+  # Returns :mix.
   #
   def self.detect_line_ending_of_text(text)
-    line_endings = text.split(/[^\r\n\v\f\u2028\u2029\u0085]/)
+    line_endings = text.split(/[^#{EOL_CHARACTERS.join('')}]/)
       .reject { |ending| ending == '' }
       .map { |ending| ending.gsub(/(.+?)(\1)+/m, '\1') }
       .uniq
@@ -33,28 +47,7 @@ module LineDetector
     if len == 0
       :none
     elsif len == 1
-      case line_endings.first
-      when "\n"
-        :lf
-      when "\r\n"
-        :crlf
-      when "\n\r"
-        :lfcr
-      when "\r"
-        :cr
-      when "\v"
-        :vt
-      when "\f"
-        :ff
-      when "\u2028"
-        :ls
-      when "\u2029"
-        :ps
-      when "\u0085"
-        :nel
-      else
-        :unknown
-      end
+      EOL2NAME[line_endings.first] || :unknown
     else
       :mix
     end
